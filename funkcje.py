e38873987c33f059976615f3c05220411b673a93
mbazych <44398475+mbazych@users.noreply.github.com>
2019-04-18 21:50:56 +0200

    gf

--- funkcje.py
+++ funkcje.py
@@ -1,13 +1,14 @@
 import mysql.connector
 
 
-def dodajKontakt():
+def conn():
+    try:
+        mydb = mysql.connector.connect(
+          host="localhost",
+          user="root",
+          passwd=""
+          )
+        cur = mydb.cursor()
 
-
-def usunKontakt():
-
-
-def wyswietlKontakty():
-
-
-def edytujKontakt():
+    except ConnectionError:
+        print("BŁĄD")
